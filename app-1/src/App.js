8ba82cdce73331f231fa53db322cc71a1b134c36
wickwickyiii <wickham.eaw@gmail.com>
2018-08-13 23:06:07 -0500

    8/13 afternoon

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -8,34 +8,8 @@ import "./App.css";
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      // input: "",
-      // food: [
-      //   "spaghetti",
-      //   "ice cream",
-      //   "sushi",
-      //   "cheese",
-      //   "eggs",
-      //   "milk",
-      //   "coffee",
-      //   "syrup",
-      //   "wine"
-      // ],
-      // filteredFood: []
-    };
-    // this.filter = this.filter.bind(this);
+    this.state = {};
   }
-
-  // filter(e) {
-  //   let newArr = [];
-  //   this.setState({ input: e.target.value });
-  //   for (var i = 0; this.state.food.length; i++) {
-  //     if (this.state.food.includes(e)) {
-  //       newArr.push(this.state.food[i]);
-  //     }
-  //   }
-  //   this.setState({ filteredFood: newArr });
-  // }
 
   render() {
     return (
--- app-1/src/components/Array.js
+++ app-1/src/components/Array.js
@@ -1,33 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class Array extends Component {
-    constructor (){
-        super();
-            this.state = {
-                food: ['spaghetti','ice cream','sushi','bologna','cheese','syrup','coffee','milk','eggs','steak','chips','fish','chicken','wine']
-            }
-            // this.ownLine = this.ownLine.bind(this)
-    }
+  constructor() {
+    super();
+    this.state = {
+      food: [
+        "spaghetti",
+        "ice cream",
+        "sushi",
+        "bologna",
+        "cheese",
+        "syrup",
+        "coffee",
+        "milk",
+        "eggs",
+        "steak",
+        "chips",
+        "fish",
+        "chicken",
+        "wine"
+      ]
+    };
+  }
 
-    // ownLine (arr) {
-    //     for (var i=0; i<arr.length; i++){
-    //         <div> {arr[i]} </div>
-    //     }
-    // }
-    
+  render() {
+    let ownLine = this.state.food.map((e, i, arr) => {
+      return <p> {this.state.food[i]} </p>;
+    });
 
-    render (){
-        let ownLine = this.state.food.map((e,i,arr) => {
-            return <p> {this.state.food[i]} </p>
-        })
-
-        return (
-            <div>
-                {/* <h2> {JSON.stringify(this.ownLine(this.state.food))} </h2> */}
-                <div> {ownLine} </div>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <div> {ownLine} </div>
+      </div>
+    );
+  }
 }
 
 export default Array;
