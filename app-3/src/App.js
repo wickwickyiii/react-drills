c4078bb51aea9c7eee4574bd29e77ad86811ba4f
wickwickyiii <wickham.eaw@gmail.com>
2018-08-20 17:32:10 -0500

    8/20 update

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: '',
+      foods: ["spaghetti", "ice cream", "bologna", "coffee", "cheese", "sushi"]
+    }
+  }
+
+  handleChange(e) {
+    this.setState({ message: e.target.value })
+  }
+
+
   render() {
+
+    let groceryList = this.state.foods.filter((e, i) => {
+      return e.includes(this.state.message);
+    })
+      .map((e, i) => {
+        return (
+          <h2 key={i}>{e}</h2>
+        )
+      })
+
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <input onChange={e => this.handleChange(e)}></input>
+
+        <div>
+          {groceryList}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
