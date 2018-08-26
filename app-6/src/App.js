2c93b813ea5df80b126c42c36525a6e4d4610d94
wickwickyiii <wickham.eaw@gmail.com>
2018-08-26 17:02:45 -0500

    8/26 app-6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import Todo from "./components/Todo";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks: [],
+      newTask: ""
+    };
+  }
+
+  handleChange(e) {
+    this.setState({ newTask: e.target.value });
+  }
+
+  handleClick() {
+    this.setState({
+      tasks: [...this.state.tasks, this.state.newTask],
+      newTask: ""
+    });
+  }
+
   render() {
+    let list = this.state.tasks.map((e, i) => {
+      return (
+        <p>
+          <Todo key={i} listItem={e} />
+        </p>
+      );
+    });
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <header>
+          <h1>My to-do list:</h1>
+        </header>
+        <div>
+          <input
+            onChange={e => this.handleChange(e)}
+            placeholder="Enter New Task"
+          />
+          <button onClick={() => this.handleClick()}>Add</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>{list}</div>
       </div>
     );
   }
