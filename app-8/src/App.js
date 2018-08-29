20c8c4aed1f2be47c6bdb1c51f8b09cef25b988b
wickwickyiii <wickham.eaw@gmail.com>
2018-08-28 20:20:19 -0500

    8/28 react-drill-8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,9 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      person: {}
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://swapi.co/api/people/1/")
+      .then(res =>
+        this.setState({
+          person: res.data
+        })
+      )
+      .catch(err => console.log(err));
+  }
+
   render() {
+    console.log(this.state.person);
+    let luke = this.state.person.name;
+    let height = this.state.person.height;
+    let hairColor = this.state.person.hair_color;
+    let eyeColor = this.state.person.eye_color;
+
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +38,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <p>Name: {luke}</p>
+          <p>Height: {height}</p>
+          <p>Hair Color: {hairColor}</p>
+          <p>Eye Color: {eyeColor}</p>
+        </div>
       </div>
     );
   }
